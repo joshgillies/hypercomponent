7a6840e4240236843cf2bc7f3fbb7a074efd2265
Josh Gillies <github@josh.gl>
2017-08-16 20:27:55 +1000

    Reduce bundle size (#14)
    - Inline xtend module
    - Assign frequently accessed properties to variables to aid minification

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var PicoComponent = require('picocomponent')
 var viperHTML = require('viperhtml')
-var extend = require('xtend')
+
+function extend (target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i]
+
+    for (var key in source) {
+      target[key] = source[key]
+    }
+  }
+
+  return target
+}
 
 function HyperComponent (props) {
   if (this.connectedCallback) this.connect = this.connectedCallback
@@ -10,35 +21,37 @@ function HyperComponent (props) {
   this.state = this.defaultState || {}
 }
 
-HyperComponent.prototype = Object.create(PicoComponent.prototype)
-HyperComponent.prototype.constructor = HyperComponent
+extend((HyperComponent.prototype = Object.create(PicoComponent.prototype)), {
+  constructor: HyperComponent,
+  handleEvent: handleEvent,
+  render: render,
+  setState: setState
+})
 
-HyperComponent.prototype.handleEvent = function handleEvent (event) {
+function handleEvent (event) {
   this['on' + event.type](event)
 }
 
-HyperComponent.prototype.render = function render (node) {
+function render (node) {
   var self = this
   this.el = this.renderCallback(
     this._wire || (self._wire = function wire () {
       var args = arguments
-      var isStatic = args[0] && args[0].raw
+      var value = args[0]
+      var isStatic = value && value.raw
+
       if (args.length > 1) {
         if (isStatic) {
           return (node ? viperHTML.bind(node) : viperHTML.wire(self)).apply(viperHTML, args)
         }
         return viperHTML.wire.apply(viperHTML, args)
       }
-      switch (typeof args[0]) {
-        case 'string':
-          return viperHTML.wire(self, args[0])
-        case 'object':
-          if (isStatic) {
-            return viperHTML.wire(self).apply(viperHTML, args)
-          }
-          return viperHTML.wire(args[0])
-        default:
-          return viperHTML.wire(self)
+
+      if (typeof value === 'object') {
+        if (isStatic) {
+          return viperHTML.wire(self).apply(viperHTML, args)
+        }
+        return viperHTML.wire(value)
       }
     }),
     this._component || (function (componentCache) {
@@ -65,8 +78,8 @@ HyperComponent.prototype.render = function render (node) {
   return this.el
 }
 
-HyperComponent.prototype.setState = function setState (state) {
-  this.state = extend(this.state, state)
+function setState (state) {
+  extend(this.state, state)
   this.render()
 }
 
